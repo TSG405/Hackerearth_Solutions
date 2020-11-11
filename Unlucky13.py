07244947b8c7a586686fae9189940600f0606f58
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-11-11 21:07:37 +0530

    Update Unlucky13.py

--- Unlucky13.py
+++ Unlucky13.py
@@ -14,3 +14,16 @@ k=int(input())
 for i in range(k):
     n=int(input())
     print(f(n)%1000000009)
+
+'''
+k=int(input())
+for i in range(k):
+    n=int(input())
+    a=[0,10,99]
+    if n>2:
+        for i in range(3,n+1):
+            temp=a[-1]
+            a[-1]=((a[-1]*10)-(a[-2]))
+            a[-2]=temp
+    print(a[-1] % 1000000009)
+'''
